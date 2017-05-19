97758aa286c1bc2ef75c1c8136e127f52373263f
L-IPSE\gparedes <gparedes@cofomo.com>
2017-05-19 11:29:00 -0400

    connec fichier

---
+++ conec.php
@@ -0,0 +1,17 @@
+<?php
+function Conec()
+{
+
+   if (!($link=mysql_connect("localhost","root","")))
+   {
+      echo "Error conec serveur.";
+      exit();
+   }
+   if (!mysql_select_db("drupal_8",$link))
+   {
+      echo "Error base de donnes.";
+      exit();
+   }
+   return $link;
+}
+?>
